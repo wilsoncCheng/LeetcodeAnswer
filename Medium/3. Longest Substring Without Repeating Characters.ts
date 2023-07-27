a4390e1da291663502ba043570c231e360fe127a
wilsoncCheng <89733416+wilsoncCheng@users.noreply.github.com>
2023-07-27 11:59:27 +0800

    3 problem solve

---
+++ Medium/3. Longest Substring Without Repeating Characters.ts
@@ -0,0 +1,16 @@
+function lengthOfLongestSubstring(s: string): number {
+    if (!s) return 0
+    const max = new Set([1])
+    let dic = ''
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i]
+        const ind = dic.indexOf(char)
+        if (ind == -1) {
+            dic += char
+            max.add(dic.length)
+        } else {
+            dic = dic.slice(ind + 1).concat(char)
+        }
+    }
+    return Math.max(...(max))
+};
